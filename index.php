d8a27b56c25849e47e37381623a1edc379b2a61d
ahmad-alif <61868297+ahmad-alif@users.noreply.github.com>
2020-03-09 19:04:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -120,7 +120,7 @@ include('getdata.php');
 
           <div class="col-lg-5 col-md-6">
             <div class="about-img">
-              <img src="img/bg.jpg" alt="">
+              <img src="img/store.png" alt="">
             </div>
           </div>
 
@@ -128,21 +128,15 @@ include('getdata.php');
             <div class="about-content">
               <h2>Tentang Toko Kami</h2>
               <hr>
-              <h3>Toko kelontong terlengkap di Karanganyar</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua.</p>
-              <p>Aut dolor id. Sint aliquam consequatur ex ex labore. Et quis qui dolor nulla dolores neque. Aspernatur
-                consectetur omnis numquam quaerat. Sed fugiat nisi. Officiis veniam molestiae. Et vel ut quidem alias
-                veritatis repudiandae ut fugit. Est ut eligendi aspernatur nulla voluptates veniam iusto vel quisquam. Fugit
-                ut maxime incidunt accusantium totam repellendus eum error. Et repudiandae eum iste qui et ut ab alias.</p>
+              <h3>Toko kelontong terlengkap di Karanganyar</h3><br>
+              <p>Awal bulan bingung mau belanja kemana? Belanja di toko kami saja. Banyak sekali barang yg tersedia di toko kami.</p>
+              <p>“Dapatkan jaminan 100% uang kembali jika produk yang Anda terima tidak sesuai foto dan deskripsi kami.”</p>
               <ul>
-                <li><i class="ion-android-checkmark-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                <li><i class="ion-android-checkmark-circle"></i> Aman
                 </li>
-                <li><i class="ion-android-checkmark-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.
+                <li><i class="ion-android-checkmark-circle"></i> Murah
                 </li>
-                <li><i class="ion-android-checkmark-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                  Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla
-                  pariatur.</li>
+                <li><i class="ion-android-checkmark-circle"></i> Terpercaya</li>
               </ul>
             </div>
           </div>
@@ -150,7 +144,6 @@ include('getdata.php');
       </div>
 
     </section><!-- #about -->
-
 
     <!--==========================
       Services Section
